9d83476845d054ac98dd581ab8ae4dab12ed1bc8
teresaramirezharo <47835481+teresaramirezharo@users.noreply.github.com>
2021-04-20 12:10:09 +0200

    Update Caja.cpp

--- Caja.cpp
+++ Caja.cpp
@@ -2,16 +2,16 @@
 #include "freeglut.h"
 
 Caja::Caja() {
-	suelo.setposicion(-10,0,10,0);
+	suelo.setpos(-10,0,10,0);
 	suelo.setcolor(0.0f, 105.0f, 0.0f);
 	
-	techo.setposicion(-10, 15, 10, 15);
+	techo.setpos(-10, 15, 10, 15);
 	techo.setcolor(0.0f, 105.0f, 0.0f);
 	
-	pared_dcha.setposicion(-10, 0, -10, 15);
+	pared_dcha.setpos(-10, 0, -10, 15);
 	pared_dcha.setcolor(0.0f, 255.0f, 100.0f);
 	
-	pared_izq.setvalores( 10, 0, 10, 15);
+	pared_izq.setpos( 10, 0, 10, 15);
 	pared_izq.setcolor(0.0f, 255.0f,100.0f);
 }
 void Caja::dibuja()
