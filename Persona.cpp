838486fb9c30f2334a35386fc135c1793d6419f7
teresaramirezharo <47835481+teresaramirezharo@users.noreply.github.com>
2021-04-30 20:12:38 +0200

    Update Persona.cpp

--- Persona.cpp
+++ Persona.cpp
@@ -1,23 +1,33 @@
 #include "Persona.h"
 #include "freeglut.h"
-Persona::Persona() {
+Persona::Persona():posicion(0,0), velocidad(0,0), aceleracion(0,0) {
 	altura = 1.8f;
-	posicion = {};
-	velocidad = {};
-	aceleracion = {};
 }
 void Persona::dibuja()
 {
 	glPushMatrix();
 	glTranslatef(posicion.x, posicion.y, 0);
-	glColor3f(0.0f, 0.0f, 1.0f);
+	glColor3f(1.0f, 0.0f, 0.0f);
 	glutSolidSphere(altura, 20, 20);
 	glPopMatrix();
 }
+void Persona::setVel(float vx, float vy)
+{
+	velocidad.x = vx;
+	velocidad.y = vy;
+}
 void Persona::mueve(float t)
 {
-	posicion.x = posicion.x + velocidad.x * t + 0.5f * aceleracion.x * t * t;
-	posicion.y = posicion.y + velocidad.y * t + 0.5f * aceleracion.y * t * t;
-	velocidad.x = velocidad.x + aceleracion.x * t;
-	velocidad.y = velocidad.y + aceleracion.y * t;
+	posicion = posicion + velocidad * t + aceleracion * (0.5f * t * t);
+	velocidad = velocidad + aceleracion * t;
+}
+
+Vector2D Persona::getposicion()
+{
+	Vector2D p = posicion;
+	return p;
+}
+void Persona::setposicion(float px, float py) {
+	posicion.x = px;
+	posicion.y = py;
 }
